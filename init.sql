69b3454949d0a7366a05f6783bcddc86d7763ac4
witenberg <jakub.witenberg@gmail.com>
2025-03-21 01:36:41 +0100

    db refactor - changed products storage

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.2
 -- Dumped by pg_dump version 17.2
 
--- Started on 2025-03-19 12:01:18
+-- Started on 2025-03-21 01:34:16
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,9 +19,9 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE IF EXISTS "SimpleInvoices";
+DROP DATABASE "SimpleInvoices";
 --
--- TOC entry 4870 (class 1262 OID 16388)
+-- TOC entry 4847 (class 1262 OID 16388)
 -- Name: SimpleInvoices; Type: DATABASE; Schema: -; Owner: postgres
 --
 
@@ -45,7 +45,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 4871 (class 0 OID 0)
+-- TOC entry 4848 (class 0 OID 0)
 -- Name: SimpleInvoices; Type: DATABASE PROPERTIES; Schema: -; Owner: postgres
 --
 
@@ -103,7 +103,7 @@ CREATE SEQUENCE public.clients_clientid_seq
 ALTER SEQUENCE public.clients_clientid_seq OWNER TO postgres;
 
 --
--- TOC entry 4872 (class 0 OID 0)
+-- TOC entry 4849 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: clients_clientid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -112,7 +112,7 @@ ALTER SEQUENCE public.clients_clientid_seq OWNED BY public.clients.clientid;
 
 
 --
--- TOC entry 225 (class 1259 OID 16498)
+-- TOC entry 223 (class 1259 OID 16498)
 -- Name: invoices_invoiceid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -127,7 +127,7 @@ CREATE SEQUENCE public.invoices_invoiceid_seq
 ALTER SEQUENCE public.invoices_invoiceid_seq OWNER TO postgres;
 
 --
--- TOC entry 226 (class 1259 OID 16522)
+-- TOC entry 224 (class 1259 OID 16522)
 -- Name: invoices; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -146,6 +146,7 @@ CREATE TABLE public.invoices (
     acceptcreditcards boolean DEFAULT false,
     acceptpaypal boolean DEFAULT false,
     subscriptionid integer,
+    products jsonb DEFAULT '[]'::jsonb NOT NULL,
     CONSTRAINT invoices_language_check CHECK (((language)::text = ANY ((ARRAY['Polski'::character varying, 'English'::character varying, 'Deutsch'::character varying, 'Français'::character varying])::text[]))),
     CONSTRAINT invoices_status_check CHECK (((status)::text = ANY (ARRAY[('Draft'::character varying)::text, ('Sent'::character varying)::text, ('Paid'::character varying)::text, ('Refunded'::character varying)::text, ('Deleted'::character varying)::text])))
 );
@@ -154,7 +155,7 @@ CREATE TABLE public.invoices (
 ALTER TABLE public.invoices OWNER TO postgres;
 
 --
--- TOC entry 224 (class 1259 OID 16480)
+-- TOC entry 222 (class 1259 OID 16480)
 -- Name: logs; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -169,7 +170,7 @@ CREATE TABLE public.logs (
 ALTER TABLE public.logs OWNER TO postgres;
 
 --
--- TOC entry 223 (class 1259 OID 16479)
+-- TOC entry 221 (class 1259 OID 16479)
 -- Name: logs_logid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -185,8 +186,8 @@ CREATE SEQUENCE public.logs_logid_seq
 ALTER SEQUENCE public.logs_logid_seq OWNER TO postgres;
 
 --
--- TOC entry 4873 (class 0 OID 0)
--- Dependencies: 223
+-- TOC entry 4850 (class 0 OID 0)
+-- Dependencies: 221
 -- Name: logs_logid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -194,76 +195,7 @@ ALTER SEQUENCE public.logs_logid_seq OWNED BY public.logs.logid;
 
 
 --
--- TOC entry 222 (class 1259 OID 16426)
--- Name: products; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.products (
-    productid integer NOT NULL,
-    userid integer NOT NULL,
-    name character varying(255) NOT NULL
-);
-
-
-ALTER TABLE public.products OWNER TO postgres;
-
---
--- TOC entry 221 (class 1259 OID 16425)
--- Name: products_productid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.products_productid_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.products_productid_seq OWNER TO postgres;
-
---
--- TOC entry 4874 (class 0 OID 0)
--- Dependencies: 221
--- Name: products_productid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.products_productid_seq OWNED BY public.products.productid;
-
-
---
--- TOC entry 227 (class 1259 OID 16547)
--- Name: productsoninvoice; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.productsoninvoice (
-    productid integer NOT NULL,
-    invoiceid integer NOT NULL,
-    amount numeric DEFAULT 0.00 NOT NULL,
-    quantity integer DEFAULT 1 NOT NULL
-);
-
-
-ALTER TABLE public.productsoninvoice OWNER TO postgres;
-
---
--- TOC entry 230 (class 1259 OID 24800)
--- Name: productsonsubscription; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.productsonsubscription (
-    subscriptionid integer NOT NULL,
-    productid integer NOT NULL,
-    amount numeric DEFAULT 0.00 NOT NULL,
-    quantity integer DEFAULT 1 NOT NULL
-);
-
-
-ALTER TABLE public.productsonsubscription OWNER TO postgres;
-
---
--- TOC entry 229 (class 1259 OID 24778)
+-- TOC entry 226 (class 1259 OID 24778)
 -- Name: subscriptions; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -284,6 +216,7 @@ CREATE TABLE public.subscriptions (
     end_date date,
     status character varying(20) NOT NULL,
     next_invoice date,
+    products jsonb DEFAULT '[]'::jsonb NOT NULL,
     CONSTRAINT subscriptions_frequency_check CHECK (((frequency)::text = ANY (ARRAY[('Weekly'::character varying)::text, ('Every 2 weeks'::character varying)::text, ('Every 4 weeks'::character varying)::text, ('Monthly'::character varying)::text, ('Quarterly'::character varying)::text, ('Every 6 months'::character varying)::text, ('Yearly'::character varying)::text]))),
     CONSTRAINT subscriptions_status_check CHECK (((status)::text = ANY (ARRAY[('Active'::character varying)::text, ('Paused'::character varying)::text, ('Deleted'::character varying)::text])))
 );
@@ -292,7 +225,7 @@ CREATE TABLE public.subscriptions (
 ALTER TABLE public.subscriptions OWNER TO postgres;
 
 --
--- TOC entry 228 (class 1259 OID 24777)
+-- TOC entry 225 (class 1259 OID 24777)
 -- Name: subscriptions_subscriptionid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -308,8 +241,8 @@ CREATE SEQUENCE public.subscriptions_subscriptionid_seq
 ALTER SEQUENCE public.subscriptions_subscriptionid_seq OWNER TO postgres;
 
 --
--- TOC entry 4875 (class 0 OID 0)
--- Dependencies: 228
+-- TOC entry 4851 (class 0 OID 0)
+-- Dependencies: 225
 -- Name: subscriptions_subscriptionid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -356,7 +289,7 @@ CREATE SEQUENCE public.users_userid_seq
 ALTER SEQUENCE public.users_userid_seq OWNER TO postgres;
 
 --
--- TOC entry 4876 (class 0 OID 0)
+-- TOC entry 4852 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: users_userid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -365,7 +298,7 @@ ALTER SEQUENCE public.users_userid_seq OWNED BY public.users.userid;
 
 
 --
--- TOC entry 4668 (class 2604 OID 16413)
+-- TOC entry 4658 (class 2604 OID 16413)
 -- Name: clients clientid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -373,7 +306,7 @@ ALTER TABLE ONLY public.clients ALTER COLUMN clientid SET DEFAULT nextval('publi
 
 
 --
--- TOC entry 4670 (class 2604 OID 16483)
+-- TOC entry 4659 (class 2604 OID 16483)
 -- Name: logs logid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -381,15 +314,7 @@ ALTER TABLE ONLY public.logs ALTER COLUMN logid SET DEFAULT nextval('public.logs
 
 
 --
--- TOC entry 4669 (class 2604 OID 16429)
--- Name: products productid; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.products ALTER COLUMN productid SET DEFAULT nextval('public.products_productid_seq'::regclass);
-
-
---
--- TOC entry 4679 (class 2604 OID 24781)
+-- TOC entry 4667 (class 2604 OID 24781)
 -- Name: subscriptions subscriptionid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -397,7 +322,7 @@ ALTER TABLE ONLY public.subscriptions ALTER COLUMN subscriptionid SET DEFAULT ne
 
 
 --
--- TOC entry 4666 (class 2604 OID 16399)
+-- TOC entry 4656 (class 2604 OID 16399)
 -- Name: users userid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -405,7 +330,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN userid SET DEFAULT nextval('public.us
 
 
 --
--- TOC entry 4695 (class 2606 OID 16419)
+-- TOC entry 4682 (class 2606 OID 16419)
 -- Name: clients clients_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -414,7 +339,7 @@ ALTER TABLE ONLY public.clients
 
 
 --
--- TOC entry 4697 (class 2606 OID 16417)
+-- TOC entry 4684 (class 2606 OID 16417)
 -- Name: clients clients_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -423,7 +348,7 @@ ALTER TABLE ONLY public.clients
 
 
 --
--- TOC entry 4703 (class 2606 OID 16535)
+-- TOC entry 4688 (class 2606 OID 16535)
 -- Name: invoices invoices_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -432,7 +357,7 @@ ALTER TABLE ONLY public.invoices
 
 
 --
--- TOC entry 4701 (class 2606 OID 16488)
+-- TOC entry 4686 (class 2606 OID 16488)
 -- Name: logs logs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -441,34 +366,7 @@ ALTER TABLE ONLY public.logs
 
 
 --
--- TOC entry 4699 (class 2606 OID 16433)
--- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.products
-    ADD CONSTRAINT products_pkey PRIMARY KEY (productid);
-
-
---
--- TOC entry 4705 (class 2606 OID 16555)
--- Name: productsoninvoice productsoninvoice_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.productsoninvoice
-    ADD CONSTRAINT productsoninvoice_pkey PRIMARY KEY (productid, invoiceid);
-
-
---
--- TOC entry 4709 (class 2606 OID 24808)
--- Name: productsonsubscription productsonsubscription_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.productsonsubscription
-    ADD CONSTRAINT productsonsubscription_pkey PRIMARY KEY (subscriptionid, productid);
-
-
---
--- TOC entry 4707 (class 2606 OID 24789)
+-- TOC entry 4690 (class 2606 OID 24789)
 -- Name: subscriptions subscriptions_new_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -477,7 +375,7 @@ ALTER TABLE ONLY public.subscriptions
 
 
 --
--- TOC entry 4691 (class 2606 OID 16408)
+-- TOC entry 4678 (class 2606 OID 16408)
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -486,7 +384,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4693 (class 2606 OID 16406)
+-- TOC entry 4680 (class 2606 OID 16406)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -495,7 +393,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4710 (class 2606 OID 16420)
+-- TOC entry 4691 (class 2606 OID 16420)
 -- Name: clients clients_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -504,7 +402,7 @@ ALTER TABLE ONLY public.clients
 
 
 --
--- TOC entry 4716 (class 2606 OID 24795)
+-- TOC entry 4695 (class 2606 OID 24795)
 -- Name: subscriptions fk_clients; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -513,25 +411,7 @@ ALTER TABLE ONLY public.subscriptions
 
 
 --
--- TOC entry 4718 (class 2606 OID 24814)
--- Name: productsonsubscription fk_product; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.productsonsubscription
-    ADD CONSTRAINT fk_product FOREIGN KEY (productid) REFERENCES public.products(productid) ON DELETE CASCADE;
-
-
---
--- TOC entry 4719 (class 2606 OID 24809)
--- Name: productsonsubscription fk_subscription; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.productsonsubscription
-    ADD CONSTRAINT fk_subscription FOREIGN KEY (subscriptionid) REFERENCES public.subscriptions(subscriptionid) ON DELETE CASCADE;
-
-
---
--- TOC entry 4717 (class 2606 OID 24790)
+-- TOC entry 4696 (class 2606 OID 24790)
 -- Name: subscriptions fk_users; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -540,7 +420,7 @@ ALTER TABLE ONLY public.subscriptions
 
 
 --
--- TOC entry 4713 (class 2606 OID 16536)
+-- TOC entry 4693 (class 2606 OID 16536)
 -- Name: invoices invoices_clientid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -549,7 +429,7 @@ ALTER TABLE ONLY public.invoices
 
 
 --
--- TOC entry 4714 (class 2606 OID 16541)
+-- TOC entry 4694 (class 2606 OID 16541)
 -- Name: invoices invoices_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -558,7 +438,7 @@ ALTER TABLE ONLY public.invoices
 
 
 --
--- TOC entry 4712 (class 2606 OID 16489)
+-- TOC entry 4692 (class 2606 OID 16489)
 -- Name: logs logs_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -566,25 +446,7 @@ ALTER TABLE ONLY public.logs
     ADD CONSTRAINT logs_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
 
 
---
--- TOC entry 4711 (class 2606 OID 16434)
--- Name: products products_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.products
-    ADD CONSTRAINT products_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
-
-
---
--- TOC entry 4715 (class 2606 OID 16556)
--- Name: productsoninvoice productsoninvoice_productid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.productsoninvoice
-    ADD CONSTRAINT productsoninvoice_productid_fkey FOREIGN KEY (productid) REFERENCES public.products(productid) ON DELETE CASCADE;
-
-
--- Completed on 2025-03-19 12:01:19
+-- Completed on 2025-03-21 01:34:16
 
 --
 -- PostgreSQL database dump complete
